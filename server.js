6ef87f7b55ba2a100bb332d77f03dedd1b136109
Brandon Olin <olin.brandon@gmail.com>
2020-05-07 12:12:18 -0400

    'updated-database-specs'

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-fitted-78417',
     user: 'postgres',
     password: 'supratt17',
     database: 'smartbrain'
